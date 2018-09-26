1bde161e8f2a373586d43a7c9f1b88f92b345710
Andrei Boeru <ab@cloudpiler.com>
2018-09-26 14:03:56 +0200

    run link

--- cloudpiler.rb
+++ cloudpiler.rb
@@ -2,15 +2,11 @@ class Cloudpiler < Formula
   desc "Distributed build speed booster service for Android C/C++"
   homepage "https://www.cloudpiler.com"
   url "https://www.cloudpiler.com/archive/cloudpiler-1.0.0.tar.gz"
-  sha256 "160c6c0b5ddc78044f825abc394b182d1df5cf0d85543a239e0f229176ea483c"
+  sha256 "47543ac009aa6cbf093d5c1c176f2d6f38c65375f94d492f06ba64535448a053"
   version "1.0.0"
   bottle :unneeded
   def install
   	bin.install Dir["*"]
+  	system "#{bin}/cloudpiler-link.sh"
   end
-
-  postflight do
-    system_command '/usr/local/bin/cloudpiler-link.sh'
-  end
-
 end 
