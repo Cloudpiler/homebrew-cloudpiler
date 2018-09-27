7933b83e154f92ba2655d439636f86508f08e919
Andrei Boeru <ab@cloudpiler.com>
2018-09-27 18:55:30 +0200

    update

--- cloudpiler.rb
+++ cloudpiler.rb
@@ -2,7 +2,7 @@ class Cloudpiler < Formula
   desc "Distributed build speed booster service for Android C/C++"
   homepage "https://www.cloudpiler.com"
   url "https://www.cloudpiler.com/archive/cloudpiler-1.0.0.tar.gz"
-  sha256 "47543ac009aa6cbf093d5c1c176f2d6f38c65375f94d492f06ba64535448a053"
+  sha256 "5c8ed0e682bd74eaf5f039796a4ae5f43adeb69e0b2b9fa74bd5699ff54ff79f"
   version "1.0.0"
   bottle :unneeded
   def install
