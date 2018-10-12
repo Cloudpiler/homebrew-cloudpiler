a9aaf6d1ef0e823e7dfc5e32ded06367b9f074d8
Andrei Boeru <ab@cloudpiler.com>
2018-10-12 17:42:23 +0200

    update

--- cloudpiler.rb
+++ cloudpiler.rb
@@ -2,11 +2,12 @@ class Cloudpiler < Formula
   desc "Distributed build speed booster service for Android C/C++"
   homepage "https://www.cloudpiler.com"
   url "https://www.cloudpiler.com/archive/cloudpiler-1.0.0.tar.gz"
-  sha256 "b738a0d536c7dbdcd3797413fd32d7c4b5582c38ca4e6bc9e64d28e0403d8e79"
+  sha256 "bb6b5ac6d5cf1487308afadd71a54bccad5f0abff9dac5c811014f5061874ab4"
   version "1.0.0"
   bottle :unneeded
   def install
         prefix.install "cloudpiler-tunnel.key"
+        prefix.install "cloudpiler-known-hosts"
         bin.install Dir["*"]
   end
   def caveats
